3f546c71afe31ef144a91a83d3307e94ce89aedd
sepandhaghighi <sepand.haghighi@yahoo.com>
2019-11-08 16:07:47 +0330

    fix : quick help added

--- js/script.js
+++ js/script.js
@@ -124,7 +124,7 @@ function redirect(flag){
 function swalHelp(){
     swal({
     title: "Hi",
-    text: '',
+    text: '<p style="text-align:justify">Mnimi is a game of memory skill inspired by Simon electronic game. The game creates a series of lights and requires a user to repeat the sequence. If the user succeeds, the series becomes progressively longer and more complex. Once the user fails, the game is over.</p>',
     html: true,
     customClass: "swal-wide"
 });
